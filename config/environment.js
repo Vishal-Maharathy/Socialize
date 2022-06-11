7c34edad3030440446da84fad13a561d4b820a19
Vishal Maharathy <vishalmaharathy76@gmail.com>
2022-06-11 21:35:08 +0530

    removed remaining file stream elements

--- config/environment.js
+++ config/environment.js
@@ -1,14 +1,5 @@
 const fs = require('fs')
-const rfs = require('rotating-file-stream')
 const path = require('path')
-
-
-const logDirectory = path.join(__dirname, '../production_logs')
-fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
-const accessLogStream = rfs.createStream('access.log', {
-    interval: '1d',
-    path: logDirectory
-})
 
 const development = {
     name: 'development',
