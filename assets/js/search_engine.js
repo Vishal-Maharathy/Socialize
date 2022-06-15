ecd0d1523b3ccc1f9e1c8ba2cf2f3e40153b1a9d
Vishal Maharathy <vishalmaharathy76@gmail.com>
2022-06-16 03:14:00 +0530

    search button for desktop mode added

--- assets/js/search_engine.js
+++ assets/js/search_engine.js
@@ -12,13 +12,14 @@
         <div class="wave"></div>
         <div class="wave"></div>
     </div>
-    `
-    
+    ` 
+
     let search_button = $('#search-button')
     let userID = search_button[0].dataset.userid
     search_button[0].addEventListener('click', function(event){
         event.preventDefault();
         let searchBox = $('#search-box');
+        console.log("BRUH")
         if(searchBox[0].style.visibility == 'visible'){
             searchBox[0].style.visibility = 'hidden'
             searchBox[0].style.opacity = '0%'
@@ -65,4 +66,43 @@
         }
         
     })
+
+    // for desktop
+    let searchInputD = $('#searchBox-D>input')
+    searchInputD[0].addEventListener('keypress', (event)=>{
+        let search_term = searchInputD[0].value.trim()
+        let container = $('#container-searches-D');
+        if(event.key=="Enter"){
+            container[0].style.height = '230px'
+            container[0].innerHTML = loadingDiv;
+            $.ajax({
+                method: 'post',
+                data: {
+                    userID: userID,
+                    searchUser: search_term
+                },
+                url: '/searchEngine',
+                success: (data)=>{
+                    container[0].innerHTML = '<ul id="close-searchBox">Close</ul>'
+                    if(data.users.length == 0){
+                        container[0].innerHTML = noresult();
+                    }
+                    for(user of data.users){
+                        container[0].innerHTML += userListDiv(user)
+                    }
+                    createCloseButton();
+                }
+            })
+        }
+    })
+    let createCloseButton = function(){
+        let closeSearchBox = $('#close-searchBox')
+        closeSearchBox[0].style.color = '#9a25af'
+        closeSearchBox[0].addEventListener('click', (event)=>{
+        event.preventDefault();
+        console.log("Bruh")
+        let container = $('#container-searches-D');
+        container[0].style.height = '0px'
+    })
+    }
 }
