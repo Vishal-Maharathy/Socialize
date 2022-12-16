1d87d9596fadfe8ac34ab8327611d0dc72167528
Vishal Maharathy <81948965+Vishal-Maharathy@users.noreply.github.com>
2022-12-16 22:00:45 +0530

    Update like_controller.js

--- controllers/like_controller.js
+++ controllers/like_controller.js
@@ -3,7 +3,7 @@ const Comment = require('../models/comments')
 const Post = require('../models/post');
 const User = require('../models/sign_up')
 let io_client = require('socket.io-client')
-let socket = io_client('https://socialize-soc.herokuapp.com')
+let socket = io_client('https://socialize-u707.onrender.com')
 
 module.exports.toggleLike = async function(req, res){
     // below three params are for socket.io live like sent{
